972bf994cec912b4414ec7f0739d140a7a54337a
Etienne-bdt <etienne.bardet@gmail.com>
2024-05-14 12:18:20 +0200

    new mask on separate file (easier to process loss)

--- Julia/deep-decoder-inpainting.jl
+++ Julia/deep-decoder-inpainting.jl
@@ -1,26 +1,27 @@
 using Flux, Images, Random, Plots, CUDA
 
-k = 320
-max_epochs = 10000
-img = load("./img/astronaut_inpainting.jpg")
+k = 256
+max_epochs = 5000
+img = load("./img/astronaut.jpg")
 #img_noisy = img + 0.25*(rand(eltype(img), size(img)) .- RGB(0.5,0.5,0.5))
 #plot(img_noisy)
 
 
+
+mask_img = load("./img/mask.jpg")
+arr_mask = cat([mask_img for _ in 1:3]..., dims=3) 
+arr_mask = reshape(arr_mask, size(arr_mask)..., 1)
+arr_mask = convert(Array{Float32}, arr_mask)
+
 arr_img_noisy = permutedims(channelview(img), (2,3,1))
 arr_img_noisy = reshape(arr_img_noisy, size(arr_img_noisy)..., 1)
-
 #Convert arr_img_noisy to Float32
 arr_img_noisy = convert(Array{Float32}, arr_img_noisy)
+arr_img_noisy = arr_img_noisy .* arr_mask |> gpu
+arr_mask = arr_mask |> gpu
 
 data = rand(eltype(arr_img_noisy), Int(size(img,1)*(2^-6)), Int(size(img,2)*(2^-6)), k,1)
-function mask(img)
-    return convert(Array{Float32},map(x -> x == RGB(1,1,1) ? 0.0 : 1.0, img))
-end 
 
-mask_img = mask(img)
-mask_img = cat([mask(img) for _ in 1:3]..., dims=3)
-mask_img = reshape(mask_img, size(mask_img)..., 1) |> gpu
 # Define the model
 model = Chain(
     Conv((1,1), k => k;),
@@ -57,7 +58,7 @@ train_set = Flux.DataLoader((data, arr_img_noisy)) |> gpu
 
 function loss(model, x, y)
     ŷ = model(x)
-    return sum(abs2, mask_img.*(ŷ .- y))
+    return sum(abs2, y.*arr_mask .- ŷ.*arr_mask)/length(y)
 end 
 t1 = time();
 for epoch in 1:max_epochs
